0cd01ca3b7bd6c0f2edb0613069be9f21e0eb452
Michael Boyle <michael.oliver.boyle@gmail.com>
2015-01-24 18:56:14 -0500

    Enable import of main classes from galgebra

--- galgebra/__init__.py
+++ galgebra/__init__.py
@@ -2,3 +2,10 @@
 A module for geometric algebra
 
 """
+
+from __future__ import absolute_import, print_function
+
+from .ga import Ga, Sm
+from .lt import Lt, Mlt
+from .metric import Metric
+from .mv import Mv, Dop, Sdop, Pdop
